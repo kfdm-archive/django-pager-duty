0870d096486f6dcf80ee30e380bc94821bd40216
Paul Traylor <kungfudiscomonkey@gmail.com>
2014-05-04 10:09:57 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='django-pager-duty',
+    description='PagerDuty Filtering and On Call Display',
+    author='Paul Traylor',
+    url='http://github.com/kfdm/django-pager-duty/',
+    version='0.0.1',
+    packages=['pagerduty'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Framework :: Django',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+)
